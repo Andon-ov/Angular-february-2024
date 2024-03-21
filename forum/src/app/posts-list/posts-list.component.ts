89088d5a5af238e88305bab8ad2db98651ac2c8b
Andon-ov <bloodtaint@yahoo.com>
2024-03-21 11:06:00 +0200

    initial commit

---
+++ forum/src/app/posts-list/posts-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css'],
+})
+export class PostsListComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.apiService.getPosts(3).subscribe((posts) => console.log(posts));
+  }
+}
---
+++ forum/src/app/themes-list/themes-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-themes-list',
+  templateUrl: './themes-list.component.html',
+  styleUrls: ['./themes-list.component.css'],
+})
+export class ThemesListComponent implements OnInit {
+  themes = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getThemes().subscribe((themes) => {
+      console.log(themes);
+    });
+  }
+}
